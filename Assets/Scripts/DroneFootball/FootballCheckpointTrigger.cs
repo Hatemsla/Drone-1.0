65bca243244416b98335b6aa59f8797e123033a2
Hatemsla <kalashnikovjan@yandex.ru>
2023-02-03 17:50:27 +0300

    Fix yawPower set after player instantiate in drone football

--- Assets/Scripts/DroneFootball/FootballCheckpointTrigger.cs
+++ Assets/Scripts/DroneFootball/FootballCheckpointTrigger.cs
@@ -16,6 +16,7 @@ namespace DroneFootball
         private readonly Vector3 _playerDroneStartPosition = new Vector3(0, 0, -35);
         private int _playerScore;
         private int _clipIndex;
+        private float _yawPower;
         
         private void Start()
         {
@@ -38,6 +39,7 @@ namespace DroneFootball
                 
                 transform.localScale = new Vector3(currentGateScale, currentGateScale, currentGateScale);
                 _playerScore = other.GetComponent<DroneFootballCheckNode>().currentNode+1;
+                _yawPower = other.GetComponent<DroneFootballController>().yawPower;
                 _footballGate.SetNewGatePosition();
                 Destroy(other.gameObject);
                 var player = Instantiate(playerDrone, _playerDroneStartPosition, Quaternion.identity);
@@ -45,6 +47,7 @@ namespace DroneFootball
                 var playerController = player.GetComponent<DroneFootballController>();
                 playerCheckNode.currentNode = _playerScore;
                 playerController.footballController.playerCheckNode = playerCheckNode;
+                playerController.yawPower = _yawPower;
                 playerController.footballController.CheckScore();
                 _footballGate.droneFootballTransform = player.transform;
             }
